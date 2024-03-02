7e74132f22853e896093a8ea52c5af64bb3e2db9
holykremowka2137 <victoria2137@tutanota.com>
2024-03-02 13:49:10 +0100

    changed after/plugin to lua/plugins

--- after/plugin/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-require("catppuccin").setup({
-    transparent_background = true,
-    integrations = {
-        harpoon = true,
-        mason = true,
-    }
-})
-
-vim.cmd.colorscheme "catppuccin-mocha"
